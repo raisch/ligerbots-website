12009283367230a7bb39e3c18ebb6ab1e685cd0a
Zach Housman <housmanzach@gmail.com>
2023-07-26 18:02:41 -0400

    trying to create the routes

---
+++ routes/mainRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/join', (req, res) => {
+    res.render('veiws/join')
+})
+
+
+
+
+
+
+
+
+
+module.exports = router
