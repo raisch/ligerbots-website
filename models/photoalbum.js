c95d90abceaaba074eac3efb691ce38ccae6460b
Rob Raisch <62550811+rob-raisch-simplisafe@users.noreply.github.com>
2023-08-06 16:12:16 -0400

    exposed Photo object

--- models/photoalbum.js
+++ models/photoalbum.js
@@ -51,11 +51,12 @@ const PhotoSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Photo must have a location_uri'],
     validate: {
-      validator: (v) => /^https:\/\/.+\.staticflickr\.com/
+      validator: v => /^https:\/\/.+\.staticflickr\.com/
       // DANGER! Assumes ALL photos live at flickr.
     }
   }
 })
+const Photo = mongoose.model('Photo', PhotoSchema)
 
 const PhotoAlbumSchema = new mongoose.Schema({
   active: {
@@ -66,7 +67,7 @@ const PhotoAlbumSchema = new mongoose.Schema({
   year: {
     type: String,
     validate: {
-      validator: (v) => /^\d{4}$/
+      validator: v => /^\d{4}$/
     },
     required: true
   },
@@ -80,53 +81,66 @@ const PhotoAlbumSchema = new mongoose.Schema({
   photos: [PhotoSchema]
 })
 
-module.exports = mongoose.model('PhotoAlbum', PhotoAlbumSchema)
+const PhotoAlbum = mongoose.model('PhotoAlbum', PhotoAlbumSchema)
+
+module.exports = { Photo, PhotoAlbum }
 
 if (process.env.SETUP) {
   // Prime the db with some dummy photo data
 
   const MONGODB_URI = 'mongodb://localhost/blog'
 
-  const primeData = [{
-    active: true,
-    year: 2022,
-    event_name: 'The Big Event',
-    event_date: '2022-01-01',
-    photos: [
-      {
-        caption: 'WTF?',
-        location_uri: 'https://farm66.staticflickr.com/65535/53059598537_7d7e4b51e5_z.jpg'
-      }
-    ]
-  }, {
-    active: true,
-    year: 2022,
-    event_name: 'Another Big Event',
-    event_date: '2022-11-01',
-    photos: [
-      {
-        caption: 'With Pringles',
-        location_uri: 'https://farm66.staticflickr.com/65535/53060663853_22d9597791_z.jpg'
-      }, {
-        caption: 'Blue Goo',
-        location_uri: 'https://farm66.staticflickr.com/65535/53060567875_c492264465_z.jpg'
-      }
-    ]
-  }, {
-    active: true,
-    year: 2023,
-    event_name: 'Copy of Another Big Event',
-    event_date: '2023-01-01',
-    photos: [
-      {
-        caption: 'With Pringles',
-        location_uri: 'https://farm66.staticflickr.com/65535/53060663853_22d9597791_z.jpg'
-      }, {
-        caption: 'Blue Goo',
-        location_uri: 'https://farm66.staticflickr.com/65535/53060567875_c492264465_z.jpg'
-      }
-    ]
-  }]
+  const primeData = [
+    {
+      active: true,
+      year: 2022,
+      event_name: 'The Big Event',
+      event_date: '2022-01-01',
+      photos: [
+        {
+          caption: 'WTF?',
+          location_uri:
+            'https://farm66.staticflickr.com/65535/53059598537_7d7e4b51e5_z.jpg'
+        }
+      ]
+    },
+    {
+      active: true,
+      year: 2022,
+      event_name: 'Another Big Event',
+      event_date: '2022-11-01',
+      photos: [
+        {
+          caption: 'With Pringles',
+          location_uri:
+            'https://farm66.staticflickr.com/65535/53060663853_22d9597791_z.jpg'
+        },
+        {
+          caption: 'Blue Goo',
+          location_uri:
+            'https://farm66.staticflickr.com/65535/53060567875_c492264465_z.jpg'
+        }
+      ]
+    },
+    {
+      active: true,
+      year: 2023,
+      event_name: 'Copy of Another Big Event',
+      event_date: '2023-01-01',
+      photos: [
+        {
+          caption: 'With Pringles',
+          location_uri:
+            'https://farm66.staticflickr.com/65535/53060663853_22d9597791_z.jpg'
+        },
+        {
+          caption: 'Blue Goo',
+          location_uri:
+            'https://farm66.staticflickr.com/65535/53060567875_c492264465_z.jpg'
+        }
+      ]
+    }
+  ]
 
   function dieDieDie (msg) {
     console.error(msg)
@@ -142,7 +156,7 @@ if (process.env.SETUP) {
 
   async function primeDb (data) {
     await dropPhotoAlbums()
-    data.forEach(async (d) => {
+    data.forEach(async d => {
       console.log(`working on ${d}`)
       const rec = new PhotoAlbum(d)
       await rec.save()
