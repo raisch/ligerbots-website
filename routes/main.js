d422730f8e9aec21381316a2dfefe3ac79d80da8
Rob Raisch <62550811+rob-raisch-simplisafe@users.noreply.github.com>
2023-09-19 17:00:48 -0400

    fixed magic routing

--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,7 @@
 /* eslint-disable camelcase, no-unused-vars */
 /** @module */
 
-const { _, assert, stringify } = require('../lib/utils')
+const { _, assert, fs, stringify } = require('../lib/utils')
 
 const express = require('express')
 
@@ -11,9 +11,9 @@ router.get('/', async (res, req) => {
   res.render('main/home')
 })
 
-router.get('/:page', async (req, res) => {
+router.get('/content/:page', async (req, res) => {
   const page = req.params.page
-  res.renderPage(page, 'PAGE TITLE')
+  res.renderPage(page, `Content for ${page}`)
 })
 
 module.exports = router
